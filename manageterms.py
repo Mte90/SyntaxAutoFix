313917bcc2f51d2cfca0e22618aaf92fd1b9e915
Daniele Scasciafratte <mte90net@gmail.com>
2018-04-10 22:19:33 +0200

    new script

---
+++ manageterms.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import argparse, os.path, json
+
+# Parse argument
+parser = argparse.ArgumentParser(description='add new terms!')
+
+parser.add_argument('-wrong', dest="wrong", type=str, required=True)
+parser.add_argument('-right', dest="right", type=str, required=True)
+parser.add_argument('-lang', dest="lang", type=str, required=True)
+args = parser.parse_args()
+
+script_path = os.path.dirname(os.path.realpath(__file__)) + '/words/' + args.lang + '.json'
+
+filepath = open(script_path, 'r')
+data = json.load(filepath)
+if args.right in data:
+    data[args.right][data[args.right].count] = args.wrong
+else:
+    data[args.right] = [args.wrong]
+filepath = open(script_path, 'w')
+filepath.write(json.dumps(data, indent=4, sort_keys=True))
+filepath.close()
