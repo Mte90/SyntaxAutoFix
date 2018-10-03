c91761114a65271c2cfeba711e6faf4ea9858a5f
Harshvardhan Joshi <hyjoshi_14@hotmail.com>
2018-10-03 13:10:12 +0800

    Add helper functions to save and return typo data from JSON files
    The data is returned as a default dict of sets.
    This prevents duplication of typos for a particular word.
    
    The data is stored back as a dictionary of word and list of incorrect 
    words since sets are not JSON serializable.

---
+++ helpers.py
@@ -0,0 +1,23 @@
+from json import load, dumps
+from collections import defaultdict
+
+def load_to_default_dict(data):
+    """Helper function to load the typo_data as a defaultdict of sets."""
+    typo_dict = defaultdict(set)
+    typo_dict.update({word: set(incorrect_words) for word, incorrect_words in data.items()})
+    return typo_dict
+
+
+def open_typo_file(path):
+    """Open a file with typos and return the data as a defaultdict of sets."""
+    with open(path, "r") as f:
+        return load(f, object_hook=load_to_default_dict)
+
+
+def save_typo_data(path, data):
+    """Save the data as a dictionary with the values being lists instead of sets."""
+    with open(path, "w") as f:
+        f.write(
+            dumps({word: list(incorrect_words) for word, incorrect_words in data.items()},
+                  indent=4,
+                  sort_keys=True))
