2e2017c9bc15eba74e54cf74b43690434401fee3
Daniele Scasciafratte <mte90net@gmail.com>
2018-10-05 12:11:17 +0200

    little fix and gitignore

--- manageterms.py
+++ manageterms.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import argparse, os.path, json
+import argparse, os.path
 from utils.data_handlers import open_typo_file, save_typo_data
 
 # Parse argument
@@ -10,9 +10,10 @@ parser.add_argument('-right', dest="right", type=str, required=True)
 parser.add_argument('-lang', dest="lang", type=str, required=True)
 args = parser.parse_args()
 
-script_path = os.path.dirname(os.path.realpath(__file__)) + '/words/' + args.lang + '.json'
+script_path = os.path.dirname(os.path.realpath(__file__))
+lang_path = script_path + '/words/' + args.lang + '.json'
 
-typo_data = open_typo_file(script_path)
+typo_data = open_typo_file(lang_path)
 typo_data[args.right].add(args.wrong)
 
-save_typo_data(typo_data, script_path)
+save_typo_data(lang_path, typo_data)
