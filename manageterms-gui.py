efd2ae7001116254f1e18e14b863dcff922066d5
Daniele Scasciafratte <mte90net@gmail.com>
2019-03-18 12:08:45 +0100

    finished ui

--- manageterms-gui.py
+++ manageterms-gui.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os, signal, sys, glob
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QMainWindow, QApplication, QErrorMessage
 from utils.data_handlers import open_typo_file, save_typo_data
 from ui_manageterms import Ui_MainWindow
@@ -14,6 +15,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.ui.setupUi(self)
         # Connect the function with the signal
         self.ui.save.clicked.connect(self.store_new_argument)
+        self.ui.save_close.clicked.connect(self.save_close)
         # When the software are closed on console the software are closed
         signal.signal(signal.SIGINT, signal.SIG_DFL)
         self.script_path = os.path.dirname(os.path.realpath(__file__)) + '/words/'
@@ -28,14 +30,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         lang = self.ui.languages.currentText()
         # Check argument is not circular
         if right == wrong:
-            QErrorMessage().showMessage('You can’t replace a word with itself. It will create a loop.')
-#        try:
-#            lang_path = self.script_path + lang + '.json'
-#            typo_data = open_typo_file(lang_path)
-#            typo_data[right].add(wrong)
-#            save_typo_data(lang_path, typo_data)
-#        except FileNotFoundError:
-#            raise ValueError('Language ' +  lang + ' actually not avalaible.')
+            msg = QErrorMessage(self)
+            msg.setWindowModality(QtCore.Qt.WindowModal)
+            msg.showMessage('You can’t replace a word with itself. It will create a loop.')
+        
+        lang_path = self.script_path + lang + '.json'
+        typo_data = open_typo_file(lang_path)
+        typo_data[right].add(wrong)
+        save_typo_data(lang_path, typo_data)
+    
+    def save_close(self):
+        self.store_new_argument()
+        self.close()
   
 
 def main():
